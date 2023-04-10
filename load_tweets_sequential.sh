e83362ba13693ab19085e97c109ac785f58f5a45
Cynthia Hom <chom@g.hmc.edu>
2023-04-10 00:52:05 -0700

    edit sequential loading file to avoid do error

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -5,9 +5,10 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load denormalized'
 echo '================================================================================'
-time for file in $files; do
-    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1277/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
-done
+time ./load_denormalized_loop.sh "$files"
+#for file in $files; do
+#    time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1277/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+#done
 
 echo '================================================================================'
 echo 'load pg_normalized'
