c56eef18772f54adf8bd3152b8a4b07f6b144a60
Cynthia Hom <chom@g.hmc.edu>
2023-04-10 01:24:43 -0700

    fix indentation error

--- load_tweets.py
+++ load_tweets.py
@@ -91,20 +91,20 @@ def insert_tweet(connection,tweet):
         tweet: a dictionary representing the json tweet object
     '''
 
-    # skip tweet if it's already inserted
+        # skip tweet if it's already inserted
 #    sql=sqlalchemy.sql.text('''
 #    SELECT id_tweets 
 #    FROM tweets
 #    WHERE id_tweets = :id_tweets
 #    ''')
-'''    res = connection.execute(sql,{
-        'id_tweets':tweet['id'],
-        })
-    if res.first() is not None:
-        return
-    
-    connection.commit()
-''' 
+    '''    res = connection.execute(sql,{
+            'id_tweets':tweet['id'],
+            })
+        if res.first() is not None:
+            return
+        
+        connection.commit()
+    ''' 
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
     with connection.begin() as trans:
