68e97d90143f2318f76429959e30fc43480d711e
Cynthia Hom <chom@g.hmc.edu>
2023-04-10 02:24:53 -0700

    remove connection.begin() inside load_tweets

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -5,7 +5,7 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load denormalized'
 echo '================================================================================'
-time ./load_denormalized_loop.sh "$files"
+#time ./load_denormalized_loop.sh "$files"
 #for file in $files; do
 #    time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1277/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 #done
@@ -13,7 +13,7 @@ time ./load_denormalized_loop.sh "$files"
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2277/ --inputs $files
+#time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2277/ --inputs $files
 #for file in $files; do
 #    python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2277/ --inputs $file
 #done
